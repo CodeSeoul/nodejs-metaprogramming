f2732591e92f29e2c5b647aaf5b7e619f73946ed
Bryan "Beege" Berry <draco4king@gmail.com>
2020-10-25 13:40:06 +0900

    Fixed create table and added sample data

--- seed_data/book.sql
+++ seed_data/book.sql
@@ -1,4 +1,10 @@
-create table book (
-    id int not null auto_increment primary key
+create table books (
+    id int not null auto_increment primary key,
     title varchar(255) not null
 );
+
+insert into books (title) 
+values 
+    ('Programming: An Introduction'),
+    ('Software Design: Like a Boss'),
+    ('Colons: Because');
